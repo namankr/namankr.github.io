c8c7b921150c9aacfd41d1d864d6aa3e99b8150c
Naman <naman@workplacefabric.com>
2019-11-22 18:29:49 +0530

    resume changes

---
+++ precache-manifest.16153eed7921f540011997b7682cdadf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83032b5042a5785f27a1cb576d79a99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e896c93aaa1ec6717739",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "505647e19e9ca2fdfcd6",
+    "url": "/static/js/2.6e7504e4.chunk.js"
+  },
+  {
+    "revision": "e896c93aaa1ec6717739",
+    "url": "/static/js/main.2f46eb58.chunk.js"
+  },
+  {
+    "revision": "c3119f27f15aa4d06ce6",
+    "url": "/static/js/runtime-main.a82f5602.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
